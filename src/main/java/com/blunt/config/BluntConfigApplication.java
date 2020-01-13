b7cfde327ad3c4bc0a529ec4eddf6e81b81370c6
EU\kpaulpa <karpoorasundarapandian_paulpandi@trimble.com>
2020-01-13 16:26:02 +0530

    Initial Commit

---
+++ src/main/java/com/blunt/config/BluntConfigApplication.java
@@ -0,0 +1,22 @@
+package com.blunt.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class BluntConfigApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BluntConfigApplication.class, args);
+  }
+
+
+}
---
+++ src/test/java/com/blunt/config/BluntConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blunt.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BluntConfigApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
